9494dbccaa1de2e8c7ae0091370ae5c85caf1b3a
William Wiley Navarre <42005831+navarrew@users.noreply.github.com>
2025-10-17 12:27:31 -0400

    configured hatch-vcs dynamic versioning and removed version.py

--- src/markshark/__init__.py
+++ src/markshark/__init__.py
@@ -1,1 +1,10 @@
-from .version import __version__
+# src/markshark/__init__.py
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # Python <3.8 backport, but you require 3.9+, so not needed.
+    from importlib_metadata import version, PackageNotFoundError  # pragma: no cover
+
+try:
+    __version__ = version("markshark")
+except PackageNotFoundError:
+    __version__ = "0.0.0+local"
